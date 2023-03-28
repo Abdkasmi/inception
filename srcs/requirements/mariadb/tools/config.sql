fa9f9b8fc24fb8a31bf2df9ca5acbb36904fbe49
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-03-28 06:20:12 +0200

    config.sql fix

--- srcs/requirements/mariadb/tools/config.sql
+++ srcs/requirements/mariadb/tools/config.sql
@@ -1,3 +1,12 @@
+/** Clean everything to avoid default values **/
+DELETE FROM	mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+/** We installed the database with the normal authetification method in the Dockerfile,
+so we need to set the password **/
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
+
 /** Create first user, second one will be created via wordpress container */
 CREATE DATABASE $MARIADB_DB;
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
