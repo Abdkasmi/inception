75cf32443ee0b3a73a24504529dccad1ba9690bc
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-04 13:30:35 +0200

    wordpress fix

---
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,23 @@
+<?php
+define('WP_CACHE', true);
+define('DB_NAME', getenv('MARIADB_DATABASE'));
+define('DB_USER', getenv('MARIADB_USER'));
+define('DB_PASSWORD', getenv('MARIADB_PWD'));
+define('DB_HOST', getenv('MARIADB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+$table_prefix = 'wp_';
+
+define('WP_DEBUG', false);
+define('WP_DEBUG_LOG', false);
+define('WP_DEBUG_DISPLAY', false);
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+
+if ( ! defined('ABSPATH') ) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+require_once ABSPATH . 'wp-settings.php';
