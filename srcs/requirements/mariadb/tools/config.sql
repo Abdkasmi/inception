9c33b0aab7044341f305b11ba84b3507383d8b7d
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-04 11:26:20 +0200

    db fix

--- srcs/requirements/mariadb/tools/config.sql
+++ srcs/requirements/mariadb/tools/config.sql
@@ -1,7 +1,7 @@
 /** Create first user, second one will be created via wordpress container */
 CREATE DATABASE '$MARIADB_DATABASE';
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_USER_PWD';
-GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO $MARIADB_USERa@'%';
+GRANT ALL PRIVILEGES ON '$MARIADB_DATABASE'.* TO '$MARIADB_USER'@'%';
 
 /** We need to flush for the grant to be active sort of reboot*/
 /** To tell the server to reload the grant tables */
