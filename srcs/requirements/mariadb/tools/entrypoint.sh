897a3c5ffe82ba358009085f1b4a7a11009d7fe4
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-09 12:19:27 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,17 +1,6 @@
-# if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
-# 	service mysql start --datadir=/var/lib/mysql
-
-# 	echo "create $MARIADB_DATABASE"
-# 	eval "echo \"$(cat config.sql)\"" | mariadb -u root
-# 	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
-
-# 	service mysql stop --datadir=/var/lib/mysql
-# fi
-
-# echo "$MARIADB_DATABASE ready"
 chown -R mysql:mysql /var/lib/mysql
 
-if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	echo "-- Starting service"
 	service mysql start
 
@@ -19,12 +8,12 @@ if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
 	touch /var/run/mysqld/mysqlf.pid
 	mkfifo /var/run/mysqld/mysqlf.sock
 
-	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $MARIADBL_DATABASE.* TO '$MARIADB_USER'@'%';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
 
-	mysqladmin -u root password $MYSQL_ROOT_PASSWORD;
+	mysqladmin -u root password $MARIADB_ROOT_PWD;
 	
 	echo "-- Stopping service"
 	service mysql stop
