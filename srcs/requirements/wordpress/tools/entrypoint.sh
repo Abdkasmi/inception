c1fc89fa8af3ae085e34de3485eb74c906af42cd
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-12 16:29:41 +0200

    fix

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -29,7 +29,7 @@ then
 
 	# Wordpress configuration
 	wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" \
-    	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
+    	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 
 
 	wp plugin update --all 
