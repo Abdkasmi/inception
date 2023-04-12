7803399ed2544e7b15e3a3f3e2a3e4b1a1949683
Abdoullah Kasmi <abdoullah@mbpdeabdoullah.home>
2023-04-12 12:32:38 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,25 +3,24 @@ chown -R mysql:mysql /var/lib/mysql
 if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	echo "-- Starting service"
 	service mysql start
-	echo "-----------------"
 
 	mkdir -p /var/run/mysqld
-	touch /var/run/mysqld/mysqlf.pid
-	mkfifo /var/run/mysqld/mysqlf.sock
-echo "-----------------"
+	touch /var/run/mysqld/mysqld.pid
+	mkfifo /var/run/mysqld/mysqld.sock
+
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
 	mysql -u root -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD';"
 	mysql -u root -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
-echo "-----------------"
+
 	mysqladmin -u root password $MARIADB_ROOT_PWD;
-	echo "-----------------"
+
 	service mysql stop
 	echo "-- Stopping service"
 else
 	mkdir /var/run/mysqld
-	touch /var/run/mysqld/mysqlf.pid
-	mkfifo /var/run/mysqld/mysqlf.sock
+	touch /var/run/mysqld/mysqld.pid
+	mkfifo /var/run/mysqld/mysqld.sock
 fi
 
 chown -R mysql /var/run/mysqld
