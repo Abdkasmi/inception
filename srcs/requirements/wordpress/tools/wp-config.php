ed80ae36df2caec95b7f09948360dac31986ac23
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-13 14:50:05 +0200

    fix

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -14,7 +14,7 @@ define( 'DB_NAME', getenv('MARIADB_DATABASE') );
 define( 'DB_USER', getenv('MARIADB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('MARIADB_PASSWORD') );
+define( 'DB_PASSWORD', getenv('MARIADB_USER_PASSWORD') );
 
 /** MySQL hostname */
 define( 'DB_HOST', getenv('MARIADB_HOST'));
