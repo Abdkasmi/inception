7cf6d09e3be5174c165f1bdea45482f2b79fe462
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-13 10:52:42 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -2,7 +2,7 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	service mysql start --datadir=/var/lib/mysql
 
 	echo "create $MARIADB_DATABASE"
-	eval "echo \"$(cat config.sql)\"" | mariadb -u root
+	eval "echo \"$(cat config.sql)\"" | mariadb -u root -p$MARIADB_ROOT_PASSWORD
 	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
 
 	service mysql stop --datadir=/var/lib/mysql
