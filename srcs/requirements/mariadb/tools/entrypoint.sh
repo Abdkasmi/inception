822bb3d2867ef100cf050def21a6bd27bc46b86a
Abdoullah Kasmi <abdoullah@mbpdeabdoullah.home>
2023-04-13 14:36:37 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,8 +3,8 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	service mysql start
 
 	echo "create $MARIADB_DATABASE"
-	# mariadb -u root -p$MARIADB_ROOT_PASSWORD
-	# mysqladmin -u root password $MARIADB_ROOT_PASSWORD
+	mariadb -u root -p$MARIADB_ROOT_PASSWORD
+	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
 
 	mysqladmin -u root password $MARIADB_ROOT_PASSWORD shutdown
 fi
