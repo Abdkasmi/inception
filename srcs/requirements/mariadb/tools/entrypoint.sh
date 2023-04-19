33fa4b6de479fd1d43bcca254d2380f2d124f9f8
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-19 13:50:04 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# socket=/run/mysqld/mysqld.sock
-# if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
-# 	service mysql start
-
-# 	echo "create $MARIADB_DATABASE"
-# 	mariadb -u root -p$MARIADB_ROOT_PASSWORD
-# 	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
-
-# 	mysqladmin -u root password $MARIADB_ROOT_PASSWORD shutdown
-# fi
-
-# echo "$MARIADB_DATABASE ready"
-# mysqld_safe
-
 # Start the mysql service
 service mysql start
 
