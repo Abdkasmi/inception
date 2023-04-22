17eed376c2119b43c9741e2bd488f03c04297a5b
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-22 23:44:45 +0200

    fix

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -16,7 +16,7 @@ define( 'DB_COLLATE', '' );
 
 define('AUTH_KEY',         'a7D>mN+j~IF{MSWg/aF1/5+!>/tax/4V}{fnk*Z4>p!6LW+vRGOaP0&GX]i-`7nB');
 define('SECURE_AUTH_KEY',  'iKEdqL0#SL/Ao618=ZD,W%Ogu0s(zpN?nLB~Q@d-Cd7fK|Sqd%!8UT,>9+ D5wgY');
-define('LOGGED_IN_KEY',    'aMlu9G=Y-D,dk}?9g}2CS(O$a@F~eB^+Peq)WXYYrIGr|uXb^]rKQJT[&jS<S4;E'));
+define('LOGGED_IN_KEY',    'aMlu9G=Y-D,dk}?9g}2CS(O$a@F~eB^+Peq)WXYYrIGr|uXb^]rKQJT[&jS<S4;E');
 define('NONCE_KEY',        '`Q{fVW_,vNEyGeaijR3a35-^@F>S^a/(R-|<O!6>lRM`@XVa@+Ig>d=r!pY&GO5|');
 define('AUTH_SALT',        '1i%SIOPS[%H^o7ku3%z>%(I>T: ~N*#&KE{s($]2.|->SE1e/ICC8$`aE<F+Y;kK');
 define('SECURE_AUTH_SALT', '22@6,} Y-Ul@;~z*#{8g+{1gm4aB+ wQws*ky3Gpgi/E{:5!f_yJ?PEtBcNKJX)v');
