6d6636af826deb8d4c67c80c046ee12993532529
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2023-04-22 20:26:50 +0200

    fix

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -47,6 +47,8 @@
 # # --nodaemonize == keep foreground
 # php-fpm7.3 --nodaemonize
 
+#!/bin/bash
+
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 chown -R www-data:www-data /var/www/*
 chown -R 755 /var/www/*
