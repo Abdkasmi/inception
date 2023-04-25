d25908dd537b0aec92f765a4c36c0eb7c56cb71f
Abdoullah Kasmi <abkasmi@shi-r2-p1.s19.be>
2023-04-25 11:21:59 +0200

    fix

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -8,9 +8,9 @@ service mysql start
 # Insert data
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 echo '1'
-mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MARIADB_USER_PASSWORD}';"
+mysql -u root -pJoueur11 -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MARIADB_USER_PASSWORD}';"
 echo '2'
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%'; FLUSH PRIVILEGES;"
+mysql -u root -pJoueur11 -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%'; FLUSH PRIVILEGES;"
 echo '3'
 # mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 # mysql -u root -e "CREATE USER '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';FLUSH PRIVILEGES;"
